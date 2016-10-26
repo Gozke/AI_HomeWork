bdbb8fb8d454a8a50ab97ff5565dda4574699128
Gozke <Gozke@94.21.81.5>
2016-10-27 00:39:51 +0200

    Works and optimised

--- Gibbs/src/edu/bme/mit/kpt7g6/gibbs/Main.java
+++ Gibbs/src/edu/bme/mit/kpt7g6/gibbs/Main.java
@@ -22,153 +22,109 @@ public class Main {
 	private int numberOfIterations;
 	private int numberOfUsefulSamples;
 
-	
-	public Main(double beta, double alfa_u, double alfa_v, RealMatrix H) {
-		this.beta = beta;
-		this.alfa_u = alfa_u;
-		this.alfa_v = alfa_v;
+	public Main(RealMatrix H) {
+		this.alfa_u = 1;
+		this.alfa_v = 1;
 		this.H = H;
-		numberOfBurnInSamples = 20;
-		numberOfIterations = 100;
+		numberOfBurnInSamples = 1;
+		numberOfIterations = 10;
 		numberOfUsefulSamples = numberOfIterations - numberOfBurnInSamples;
 	}
-	
-	public ApproximationResult findApproximation(int I, int J, int L){
+
+	public ApproximationResult findApproximation(int I, int J, int L, double beta) {
+		this.beta = beta;
 		RealMatrix sumU = new Array2DRowRealMatrix(L, I);
 		RealMatrix sumV = new Array2DRowRealMatrix(L, J);
 		U = initializeByPrior(L, I, alfa_u);
 		V = initializeByPrior(L, J, alfa_v);
-		
-		for(int iterationIndex = 0; iterationIndex < numberOfIterations+numberOfBurnInSamples; iterationIndex++){
-			if(iterationIndex % 2 == 0){
-				U = computeNewU();
-			} else {
-				V = computeNewV();
-			}
-			//U = newU;
-			if(iterationIndex > numberOfBurnInSamples){
+
+		for (int iterationIndex = 0; iterationIndex < numberOfIterations + numberOfBurnInSamples; iterationIndex++) {
+			U = computeNewU();
+			V = computeNewV();
+
+			if (iterationIndex > numberOfBurnInSamples) {
 				sumU = sumU.add(U);
 				sumV = sumV.add(V);
 			}
 		}
-		
-		return new ApproximationResult(sumU.scalarMultiply(1.0/numberOfUsefulSamples), sumV.scalarMultiply(1.0/numberOfBurnInSamples));
+		return new ApproximationResult(sumU.scalarMultiply(1.0 / numberOfUsefulSamples),
+				sumV.scalarMultiply(1.0 / numberOfUsefulSamples));
 	}
 
-	public RealMatrix computeNewU(){
+	public RealMatrix computeNewU() {
 		RealMatrix newU = new Array2DRowRealMatrix(U.getRowDimension(), U.getColumnDimension());
-		for(int i = 0; i<U.getColumnDimension(); i++){
-			RealMatrix lambda = computeLambdaU();
+		for (int i = 0; i < U.getColumnDimension(); i++) {
+			RealMatrix lambda = computeLambda(V, alfa_u);
 			RealMatrix lambdaInverse = new LUDecomposition(lambda).getSolver().getInverse();
-			RealVector fi = computeFiU(i, lambdaInverse);
-			MultivariateNormalDistribution distribution = new MultivariateNormalDistribution(fi.toArray(), lambdaInverse.getData());
+			RealVector fi = computeFi(H.getRowVector(i), V, lambdaInverse);
+			MultivariateNormalDistribution distribution = new MultivariateNormalDistribution(fi.toArray(),
+					lambdaInverse.getData());
 			newU.setColumn(i, distribution.sample());
 		}
 		return newU;
 	}
-	
-	public RealMatrix computeNewV(){
+
+	public RealMatrix computeNewV() {
 		RealMatrix newV = new Array2DRowRealMatrix(V.getRowDimension(), V.getColumnDimension());
-		for(int j = 0; j<V.getColumnDimension(); j++){
-			RealMatrix lambda = computeLambdaV();
+		for (int j = 0; j < V.getColumnDimension(); j++) {
+			RealMatrix lambda = computeLambda(U, alfa_v);
 			RealMatrix lambdaInverse = new LUDecomposition(lambda).getSolver().getInverse();
-			RealVector fi = computeFiV(j, lambdaInverse);
-			MultivariateNormalDistribution distribution = new MultivariateNormalDistribution(fi.toArray(), lambdaInverse.getData());
+			RealVector fi = computeFi(H.getColumnVector(j), U, lambdaInverse);
+			MultivariateNormalDistribution distribution = new MultivariateNormalDistribution(fi.toArray(),
+					lambdaInverse.getData());
 			newV.setColumn(j, distribution.sample());
 		}
 		return newV;
 	}
-	
-	public RealMatrix computeLambdaU(){
-		RealMatrix sum = new Array2DRowRealMatrix(U.getRowDimension(), U.getRowDimension()); // LxL
-		for(int j = 0; j<V.getColumnDimension();j++){
-			sum = sum.add(V(j).outerProduct(V(j))); // Vj * Vj^T
+
+	public RealMatrix computeLambda(RealMatrix matrix, double alfa) {
+		return matrix.multiply(matrix.transpose()).scalarMultiply(beta).add(Utils.createDiagonalMatrixOf(matrix.getRowDimension(), alfa));
+	}
+
+	public RealVector computeFi(RealVector vectorOfH, RealMatrix otherMatrix, RealMatrix lambdaInverse) {
+		RealVector sum = otherMatrix.transpose().preMultiply(vectorOfH);
+		return lambdaInverse.operate(sum.mapMultiplyToSelf(beta));
+	}
+
+	public RealMatrix initializeByPrior(int numberOfRows, int numberOfColumns, double alpha) {
+		RealVector zeroVector = new ArrayRealVector(numberOfRows);
+		RealMatrix result = new Array2DRowRealMatrix(numberOfRows, numberOfColumns);
+		MultivariateNormalDistribution distr = new MultivariateNormalDistribution(zeroVector.toArray(),
+				Utils.createDiagonalMatrixOf(numberOfRows, 1.0 / alpha).getData());
+		for (int i = 0; i < numberOfColumns; i++) {
+			result.setColumn(i, distr.sample());
 		}
-		return sum.scalarMultiply(beta).add(Utils.createDiagonalMatrixOf(U.getRowDimension(), alfa_u));
-	}
-	
-	public RealMatrix computeLambdaV(){
-		RealMatrix sum = new Array2DRowRealMatrix(V.getRowDimension(), V.getRowDimension()); // LxL
-		for(int i = 0; i<U.getColumnDimension(); i++){
-			sum = sum.add(U(i).outerProduct(U(i)));
-		}
-		return sum.scalarMultiply(beta).add(Utils.createDiagonalMatrixOf(V.getRowDimension(), alfa_v));
-	}
-	
-	public RealVector computeFiU(int i, RealMatrix lambdaInverse){
-		RealVector sum = new ArrayRealVector(V.getRowDimension());
-		for(int j = 0; j<V.getColumnDimension(); j++){
-			sum = sum.add(V(j).mapMultiply(H(i,j))); // H_ij * Vj
-		}
-		return lambdaInverse.operate(sum.mapMultiply(beta));
-	}
-	
-	public RealVector computeFiV(int j, RealMatrix lambdaInverse){
-		RealVector sum = new ArrayRealVector(U.getRowDimension());
-		for(int i = 0; i<U.getColumnDimension(); i++){
-			sum = sum.add(U(i).mapMultiply(H(i,j)));
-		}
-		return lambdaInverse.operate(sum.mapMultiply(beta));
-	}
-	public RealVector U(int colIndex){
-		return U.getColumnVector(colIndex);
-	}
-	
-	public RealVector V(int colIndex){
-		return V.getColumnVector(colIndex);
-	}
-	
-	public double H(int row, int col){
-		return H.getEntry(row, col);
-	}
-	
-	public RealMatrix initializeByPrior(int numberOfRows, int numberOfColumns, double alpha){
 		return new Array2DRowRealMatrix(numberOfRows, numberOfColumns);
 	}
-	
-	public static class ApproximationResult{
+
+	public static class ApproximationResult {
 		public final RealMatrix U;
 		public final RealMatrix V;
-		
-		public ApproximationResult(RealMatrix U, RealMatrix V){
+
+		public ApproximationResult(RealMatrix U, RealMatrix V) {
 			this.U = U.copy();
 			this.V = V.copy();
 		}
 	}
-	
+
 	public static void main(String[] args) throws IOException {
+		long starTime = System.currentTimeMillis(); 
 		BufferedReader stdInReader = new BufferedReader(new InputStreamReader(System.in));
 		double[] vectorDimensionsAndBeta = Utils.toDoubleArray(stdInReader.readLine(), ",");
 		int I = (int) vectorDimensionsAndBeta[0];
 		int J = (int) vectorDimensionsAndBeta[1];
 		int L = (int) vectorDimensionsAndBeta[2];
 		double beta = vectorDimensionsAndBeta[3];
-		double alfa_u = 1;
-		double alfa_v = 1;
+
 		RealMatrix H = Utils.readMatrix(stdInReader, I, J);
-		Main sampler = new Main(beta, alfa_u, alfa_v, H);
-		ApproximationResult appResult = sampler.findApproximation(I, J, L);
-				
-		System.out.println(Utils.MATRIX_FORMATTER.format(appResult.U));
+		Main sampler = new Main(H);
+		ApproximationResult appResult = sampler.findApproximation(I, J, L, beta);
+
+		System.out.println(Utils.MATRIX_FORMATTER.format(appResult.U.transpose()));
 		System.out.println();
-		System.out.println(Utils.MATRIX_FORMATTER.format(appResult.V));
-		
-		
-		System.out.println();
-		RealMatrix HKalap = appResult.U.transpose().multiply(appResult.V);
-		System.out.println(Utils.MATRIX_FORMATTER.format(HKalap));
-		
-		/*
-		RealMatrix U = new Array2DRowRealMatrix(2,5);
-		U.setRow(0, new double[]{0.25,2.65,1.61,7.63,2.88});
-		U.setRow(1, new double[]{3.79,2.31,5.40,3.88,6.00});
-		
-		RealMatrix V = new Array2DRowRealMatrix(2,6);
-		V.setRow(0, new double[]{1.68,5.91,3.38,3.24,0.57,2.61});
-		V.setRow(1, new double[]{2.37,7.10,5.43,0.05,0.01,3.14});
-		System.out.println(Utils.MATRIX_FORMATTER.format(U.transpose().multiply(V)));*/
+		System.out.println(Utils.MATRIX_FORMATTER.format(appResult.V.transpose()));
+		long stopTime = System.currentTimeMillis();
+		System.out.println((stopTime-starTime)/1000.0);
 	}
-	
+
 }
-
-
--- Gibbs/src/edu/bme/mit/kpt7g6/gibbs/Utils.java
+++ Gibbs/src/edu/bme/mit/kpt7g6/gibbs/Utils.java
@@ -4,18 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Arrays;
 
-import org.apache.commons.math3.analysis.UnivariateFunction;
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.DiagonalMatrix;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealMatrixFormat;
+import org.apache.commons.math3.linear.RealMatrixPreservingVisitor;
 import org.apache.commons.math3.linear.RealVector;
 import org.apache.commons.math3.linear.RealVectorFormat;
 
 public class Utils {
 	public static final RealMatrixFormat MATRIX_FORMATTER = new RealMatrixFormat("", "", "", "", "\n", ",");
 	public static final RealVectorFormat VECTOR_FORMATTER = new RealVectorFormat("", "", ",");
+	public static final RealMatrixPreservingVisitor SQUARE_AND_SUM_VISITOR = new SquareAndAdd();
 	
 	public static RealMatrix readMatrix(BufferedReader reader, int noRows, int noColumns) throws IOException{
 		double[][] elements = new double[noRows][noColumns];
@@ -54,22 +55,27 @@ public class Utils {
 	}
 	
 	public static double calculateError(RealMatrix expected, RealMatrix actual){
-		double errorSquareSum = 0;
 		RealMatrix difference = expected.subtract(actual);
-		for(int i = 0; i<difference.getRowDimension();i++){
-			difference.setRowVector(i, difference.getRowVector(i).map(new UnivariateFunction() {
-				
-				@Override
-				public double value(double x) {
-					return x*x;
-				}
-			}));			
+		double errorSquareSum = difference.walkInOptimizedOrder(SQUARE_AND_SUM_VISITOR);
+		return errorSquareSum / (difference.getRowDimension()*difference.getColumnDimension());
+	}
+	
+	public static class SquareAndAdd implements RealMatrixPreservingVisitor {
+		private double sum;
+		
+		@Override
+		public void visit(int row, int column, double value) {
+			sum += value*value;
 		}
-		for(int i = 0; i<difference.getRowDimension(); i++){
-			for(int j = 0; j<difference.getColumnDimension(); j++){
-				errorSquareSum += difference.getEntry(i, j);
-			}
+		
+		@Override
+		public void start(int rows, int columns, int startRow, int endRow, int startColumn, int endColumn) {
+			sum = 0;
 		}
-		return errorSquareSum / difference.getColumnDimension()*difference.getColumnDimension();
+		
+		@Override
+		public double end() {
+			return sum;
+		}
 	}
 }
